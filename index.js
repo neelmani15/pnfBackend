3db740200954ac329dc71e5a284225c199a6ae8e
neelmani15 <neelmani242@gmail.com>
2024-01-12 10:26:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const _ = require('lodash');
+
+dotenv.config(); 
+
+const app = express();
+const Port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to PNF Loan Backend!');
+});
+
+app.get('/cdloans',async (req,res)=>{
+    try{
+        const url = process.env.TIGERSHEET_API_URL;
+        const headers = {
+        'Authorization': process.env.TIGERSHEET_AUTHORIZATION_TOKEN,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        };
+        const sheetId = process.env.TIGERSHEET_CDLOANS_SHEET_ID;
+        // Get criteria from request query parameters
+        const criteria = req.query.criteria || '';;
+        const cdloansRecords = await getcdloansRecords(url, headers, sheetId,criteria);
+        res.send({data:cdloansRecords})
+
+    }catch(err){
+        console.error('Error in fetching data:', err.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+async function getcdloansRecords(url, headers, sheetId,criteria) {
+    const payload = {
+      'sheet_id': sheetId,
+      'criteria': criteria,
+    };
+  
+    const response = await axios.post(url, payload, { headers });
+    console.log('All Records from Tigersheet Backend', response.data);
+  
+    return response.data.data;
+  }
+
+app.get('/emi',async (req,res)=>{
+    try{
+        const url = process.env.TIGERSHEET_API_URL;
+        const headers = {
+        'Authorization': process.env.TIGERSHEET_AUTHORIZATION_TOKEN,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        };
+        const sheetId = process.env.TIGERSHEET_EMI_SHEET_ID;
+        // Get criteria from request query parameters
+        const criteria = req.query.criteria || '';
+        const emiRecords = await getemiRecords(url, headers, sheetId,criteria);
+        res.send({data:emiRecords})
+
+    }catch(err){
+        console.error('Error in fetching data:', err.message);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+async function getemiRecords(url, headers, sheetId,criteria) {
+    const payload = {
+      'sheet_id': sheetId,
+      'criteria': criteria,
+    };
+  
+    const response = await axios.post(url, payload, { headers });
+    console.log('All Records from Tigersheet Backend', response.data);
+  
+    return response.data.data;
+  }
+
+app.listen(Port,()=>{
+    console.log(`Server is running on ${Port}`);
+});
