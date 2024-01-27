1911d2162c0fd0f7b5f43644be85745e841e625f
neelmani15 <neelmani242@gmail.com>
2024-01-27 11:44:04 +0530

    Add Truck Number in Profile Page

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const _ = require('lodash');
 
+
 dotenv.config(); 
 
 const app = express();
+const customerRouter = express.Router();
 const Port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -15,6 +17,8 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello, welcome to PNF Loan Backend!');
 });
+
+app.use('/customer', customerRouter);
 
 app.get('/cdloans',async (req,res)=>{
     try{
@@ -112,7 +116,7 @@ async function gettyreloansRecords(url, headers, sheetId,criteria) {
     return response.data.data;
 }
 
-app.get('/customer',async (req,res)=>{
+customerRouter.get('/',async (req,res)=>{
     try{
         const url = process.env.TIGERSHEET_API_URL;
         const headers = {
@@ -143,11 +147,42 @@ async function getcustomerRecords(url, headers, sheetId,criteria) {
     return response.data.data;
 }
 
+customerRouter.get('/trucks',async (req,res)=>{
+    try{
+        const url = process.env.TIGERSHEET_API_URL;
+        const headers = {
+        'Authorization': process.env.TIGERSHEET_AUTHORIZATION_TOKEN,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        };
+        const sheetId = process.env.TIGERSHEET_TRUCK_SHEET_ID;
+        // Get criteria from request query parameters
+        const criteria = req.query.criteria || '';;
+        const truckRecords = await gettruckRecords(url, headers, sheetId,criteria);
+        res.send({data:truckRecords})
+
+    }catch(err){
+        console.error('Error in fetching data:', err.message);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+async function gettruckRecords(url, headers, sheetId,criteria) {
+    const payload = {
+      'sheet_id': sheetId,
+      'criteria': criteria,
+    //   'sort':JSON.stringify([{"property":"column_85","direction":"desc"}])
+    };
+    const response = await axios.post(url, payload, { headers });
+    console.log('All Records from Tigersheet Backend', response.data);
+  
+    return response.data.data;
+}
+
 app.post("/tyre",async (req,res)=>{
     try{
         const url=process.env.TIGERSHEET_API_CREATE_URL;
         const headers={
-            'Authorization':process.env.TIGERSHEET_AUTHORIZATION_TOKEN1,
+            'Authorization':process.env.TIGERSHEET_AUTHORIZATION_TOKEN,
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         }
         const sheetId = process.env.TIGERSHEET_TYRE_LOAN_SHEET_ID;
@@ -166,42 +201,45 @@ app.post("/tyre",async (req,res)=>{
             truckNumber,
             source,
             date,
-            NoOfTrucks
+            NoOfTrucks,
+            cnfPanNumber
         } = req.body;
 
-        const data = JSON.stringify({
-            "30541":{"value":numberOfTires },
-            "30542":{"value":selectedBrand},
-            "30543":{"value":loanAmount },
-            "31495":{"value":mobilenumber},
-            "31820":{"value":FullName},
-            "31821":{"value":PanNumber},
-            "31822":{"value":AlternateMobileNumber},
-            "31854":{"value":martialStatus},
-            "31855":{"value":numchildren},
-            "31856":{"value":houseType},
-            "31857":{"value":truckNumber},
-            "31858":{"value":date},
-            "31859":{"value":source},
-            "32063":{"value":NoOfTrucks}
-        });
-
         // const data = JSON.stringify({
-        //     "806":{"value":numberOfTires },
-        //     // "30542":{"value":selectedBrand},
-        //     "805":{"value":loanAmount },
-        //     "793":{"value":mobilenumber},
-        //     "791":{"value":FullName},
-        //     "792":{"value":PanNumber},
-        //     "794":{"value":AlternateMobileNumber},
-        //     "800":{"value":martialStatus},
-        //     "801":{"value":numchildren},
-        //     "802":{"value":houseType},
-        //     "803":{"value":truckNumber},
-        //     "790":{"value":date},
-        //     "807":{"value":source},
-        //     "795":{"value":NoOfTrucks}
+        //     "30541":{"value":numberOfTires },
+        //     "30542":{"value":selectedBrand},
+        //     "30543":{"value":loanAmount },
+        //     "31495":{"value":mobilenumber},
+        //     "31820":{"value":FullName},
+        //     "31821":{"value":PanNumber},
+        //     "31822":{"value":AlternateMobileNumber},
+        //     "31854":{"value":martialStatus},
+        //     "31855":{"value":numchildren},
+        //     "31856":{"value":houseType},
+        //     "31857":{"value":truckNumber},
+        //     "31858":{"value":date},
+        //     "31859":{"value":source},
+        //     "32063":{"value":NoOfTrucks},
+        //     "32122":{"value":cnfPanNumber}
         // });
+
+        const data = JSON.stringify({
+            "791":{"value":FullName},
+            "790":{"value":date},
+            "805":{"value":loanAmount },
+            // "807":{"value":source},
+            "806":{"value":numberOfTires },
+            "792":{"value":PanNumber},
+            "793":{"value":mobilenumber},
+            "794":{"value":AlternateMobileNumber},
+            "795":{"value":NoOfTrucks},
+            "800":{"value":martialStatus},
+            "801":{"value":numchildren},
+            "802":{"value":houseType},
+            "803":{"value":truckNumber},
+            "855":{"value":selectedBrand},
+            "810":{"value":cnfPanNumber}
+        });
 
         const tyreData= await getTyreData(url,headers,sheetId,data);
 
@@ -223,6 +261,8 @@ async function getTyreData(url,headers,sheetId,data){
   
     return response.data.data;
 }
+
+
 
 app.listen(Port,()=>{
     console.log(`Server is running on ${Port}`);
