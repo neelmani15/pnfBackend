ede84d7ff9a5f09ce124a851923cdad7cbe62d1a
neelmani15 <neelmani242@gmail.com>
2024-02-07 13:15:11 +0530

    Add Customer KYC, Push Notification, Vechile

--- index.js
+++ index.js
@@ -110,8 +110,9 @@ app.get('/tyreloans',async (req,res)=>{
 app.post('/receiveToken',async (req, res) => {
     const { notificationData } = req.body;
     console.log("Notification Data",notificationData);
-    setInterval(()=>{
-        sendNotification(notificationData)
+    // await sendNotification(notificationData);
+    setInterval(async()=>{
+        await sendNotification(notificationData)
     },1000);
     // cron.schedule('0-59 * * * *',async()=>{
     //     await sendNotification(notificationData);
